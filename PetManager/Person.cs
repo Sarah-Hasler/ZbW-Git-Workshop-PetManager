148bc3745edf141733a7136c9b6a2638b71fbfb4
SarahHasler <sarahhasler@bluewin.ch>
2018-06-13 19:18:27 +0200

    feat: hashcode

--- PetManager/Person.cs
+++ PetManager/Person.cs
@@ -5,40 +5,59 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
 
-namespace PetManager {
-    public class Person {
-        public Person() {
-            this.Pets = new BindingList<Pet>();
-        }
-        public string Lastname { get; set; }
-        public string Firstname { get; set; }
+namespace PetManager
+{
+	public class Person
+	{
+		public Person()
+		{
+			this.Pets = new BindingList<Pet>();
+		}
 
-        public BindingList<Pet> Pets { get; private set; }
+		public string Lastname { get; set; }
+		public string Firstname { get; set; }
+
+		public BindingList<Pet> Pets { get; private set; }
 
 
-        public static BindingList<Person> GetDemoData() {
-            var ret = new BindingList<Person>();
-            var person = new Person() {Lastname = "Müller", Firstname = "Max"};
-            person.Pets.Add(new Pet { Name = "Cäsar", Breed = "Kanarienvogel"});
-            person.Pets.Add(new Pet { Name = "Bello", Breed = "Hund" });
-            ret.Add(person);
+		public static BindingList<Person> GetDemoData()
+		{
+			var ret = new BindingList<Person>();
+			var person = new Person() {Lastname = "Müller", Firstname = "Max"};
+			person.Pets.Add(new Pet {Name = "Cäsar", Breed = "Kanarienvogel"});
+			person.Pets.Add(new Pet {Name = "Bello", Breed = "Hund"});
+			ret.Add(person);
 
-            person = new Person() { Lastname = "Doe", Firstname = "John" };
-            person.Pets.Add(new Pet { Name = "Fleckli", Breed = "Kaninchen" });
-            person.Pets.Add(new Pet { Name = "Hoppel", Breed = "Kaninchen" });
-            person.Pets.Add(new Pet { Name = "Wau", Breed = "Hund" });
-            ret.Add(person);
+			person = new Person() {Lastname = "Doe", Firstname = "John"};
+			person.Pets.Add(new Pet {Name = "Fleckli", Breed = "Kaninchen"});
+			person.Pets.Add(new Pet {Name = "Hoppel", Breed = "Kaninchen"});
+			person.Pets.Add(new Pet {Name = "Wau", Breed = "Hund"});
+			ret.Add(person);
 
-            return ret;
-        }
+			return ret;
+		}
 
-        public override bool Equals(object obj) {
-            if (!(obj is Person)) {
-                return object.Equals(obj, this);
-            }
+		public override bool Equals(object obj)
+		{
+			if (!(obj is Person))
+			{
+				return object.Equals(obj, this);
+			}
 
-            var person = (Person) obj;
-            return string.Equals(this.Lastname, person.Lastname) && string.Equals(this.Firstname, person.Firstname);
-        }
-    }
+			var person = (Person) obj;
+			return string.Equals(this.Lastname, person.Lastname) && string.Equals(this.Firstname, person.Firstname);
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked // Overflow is fine, just wrap
+			{
+				int hash = 17;
+				hash = hash * 23 + (this.Lastname != null ? this.Lastname.GetHashCode() : 0);
+				hash = hash * 23 + (this.Firstname != null ? this.Firstname.GetHashCode() : 0);
+				hash = hash * 23 + (this.Pets != null ? this.Pets.GetHashCode() : 0);
+				return hash;
+			}
+		}
+	}
 }
